332b441bbb5860ce78ae8c2e83fb15fe4c68a3ce
Jendrik Johannes <jendrik@gradle.com>
2016-12-12 16:55:24 +0100

    Add template for source files

---
+++ gradle/SourceTemplate/app/src/main/java/gradle/example/DaggerModule.java
@@ -0,0 +1,15 @@
+package gradle.example;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class DaggerModule {
+
+    @Provides static DomainObject provideDO() {
+        return new DomainObject();
+    }
+
+    public static class DomainObject {
+    }
+}
